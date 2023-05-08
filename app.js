412fffba413c091d22f2f07ad5a556273f587006
anna <anna@rcbd.org>
2023-05-08 11:39:23 -0400

    First commit

--- app.js
+++ app.js
@@ -1,54 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
+const num1 = document.querySelector("#num1");
+const num2 = document.querySelector("#num2");
+const addBtn = document.querySelector("#add");
+const subBtn = document.querySelector("#sub");
+const output = document.querySelector("#output");
 
-  const num1 = document.querySelector('#num1')
-  const num2 = document.querySelector('#num2')
-  const addBtn = document.querySelector('#add')
-  const subBtn = document.querySelector('#sub')
-  const output = document.querySelector('#output')
+function getInputValue() {
+  const value1 = parseInt(num1.value);
+  const value2 = +num2.value;
 
-  function getInputValues() {
-    const value1 = parseInt(num1.value)
-    const value2 = +num2.value
+  return [value1, value2];
+}
 
-    return [value1, value2]
-  }
+function addHandler() {
+  const values = getInputValue();
+  const result = values[0] + values[1];
+  displayResult(result);
+}
 
+function subHandler() {
+  const values = getInputValue();
+  const result = values[0] - values[1];
+  displayResult(result);
+}
 
-  function addHandler() {
-    const values = getInputValues()
-    // console.log(values)
+function displayResult(result) {
+  output.closest(".card").getElementsByClassName.display = "block";
+  output.innerHTML = `Result ${result}`;
+}
 
-    const result = values[0] + values[1]
-
-    displayResult(result)
-  }
-
-  function subHandler() {
-    const values = getInputValues()
-
-    const result = values[0] - values[1]
-
-    displayResult(result)
-  }
-
-  function displayResult(result) {
-    output.closest('.card').style.display = 'block'
-    output.innerHTML = `Результат = ${result}`
-    console.trace()
-  }
-
-
-  // console.log('Test', addBtn)
-
-  addBtn.addEventListener('click', addHandler)
-  subBtn.addEventListener('click', subHandler)
-
-})
-
-
-// setTimeout(() => {
-//   fetch('https://jsonplaceholder.typicode.com/todos/1')
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-// }, 5000)
-
+addBtn.addEventListener("click", addHandler);
+subBtn.addEventListener("click", subHandler);
